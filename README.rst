e71b08167808ba1804f26b1bb7e904018cf8ee7b
Jens Neuhaus <kontakt@jensneuhaus.de>
2022-03-25 10:59:50 +0100

    First commit

---
+++ README.rst
@@ -0,0 +1,41 @@
+=============
+Automagically
+=============
+
+
+.. image:: https://img.shields.io/pypi/v/automagically.svg
+        :target: https://pypi.python.org/pypi/automagically
+
+.. image:: https://img.shields.io/travis/automagically-cloud/automagically.svg
+        :target: https://travis-ci.com/automagically-cloud/automagically
+
+.. image:: https://readthedocs.org/projects/automagically/badge/?version=latest
+        :target: https://automagically.readthedocs.io/en/latest/?version=latest
+        :alt: Documentation Status
+
+
+.. image:: https://pyup.io/repos/github/automagically-cloud/automagically/shield.svg
+     :target: https://pyup.io/repos/github/automagically-cloud/automagically/
+     :alt: Updates
+
+
+
+Automagically Client / SDK
+
+
+* Free software: MIT license
+* Documentation: https://automagically.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=7.0', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Jens Neuhaus",
+    author_email='jens@automagically.sh',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Automagically Client / SDK",
+    entry_points={
+        'console_scripts': [
+            'automagically=automagically.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='automagically',
+    name='automagically',
+    packages=find_packages(include=['automagically', 'automagically.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/automagically-cloud/automagically',
+    version='0.1.0',
+    zip_safe=False,
+)
