b064d550712a0286e2e825ba4dd31eddd7af876b
Jens Neuhaus <kontakt@jensneuhaus.de>
2022-03-25 11:50:17 +0100

    small changes

--- setup.py
+++ setup.py
@@ -7,16 +7,13 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
-    history = history_file.read()
-
 requirements = ['Click>=7.0', ]
 
 test_requirements = ['pytest>=3', ]
 
 setup(
     author="Jens Neuhaus",
-    author_email='jens@automagically.sh',
+    author_email='hey@automagically.cloud',
     python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -36,14 +33,14 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + '\n\n',
     include_package_data=True,
     keywords='automagically',
     name='automagically',
     packages=find_packages(include=['automagically', 'automagically.*']),
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/automagically-cloud/automagically',
+    url='https://github.com/automagically-cloud/python-client',
     version='0.1.0',
     zip_safe=False,
 )
